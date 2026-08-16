85a9a5fba6f8e60f3880d89d293ee485354453f2
Oleg Potapenko <PotapenkoOleg@gmail.com>
2026-08-15 22:48:42 -0400

    Integration tests rename

--- crates/ternary_trie/src/lib.rs
+++ crates/ternary_trie/src/lib.rs
@@ -6,6 +6,6 @@ pub use patricia_trie::PatriciaTrie;
 pub use symbol_table::{PrefixSearch, SymbolTable};
 pub use ternary_trie::TernarySearchTrie;
 
-mod integration_tests;
+mod tests_integration;
 mod tests;
 mod tests_original;
